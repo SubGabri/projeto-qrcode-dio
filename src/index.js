fbcdd4362ca4e56b58481fe00ea838fdaddb6df4
SubGabri <sub.gabri@gmail.com>
2025-08-13 23:32:37 -0300

    primeiro commit do projeto

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import prompt from "prompt";
+
+import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
+
+import createQRCode from "./services/qr-code/create.js";
+import creatPassword from "./services/password/create.js";
+
+async function main() {
+  prompt.get(promptSchemaMain, async (err, choose) => {
+    if (err) console.log(err);
+
+    if (choose.select == 1) await createQRCode();
+    if (choose.select == 2) await creatPassword();
+  });
+
+  prompt.start();
+}
+
+main();
